9c9c23c7298329e9502ba6f8653ef1fa14b0902a
Sirbinod <00np10@gmail.com>
2023-02-02 23:40:33 +0545

    fix: responsive ui

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,36 @@
+import "./style.css";
+
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="container footer-contain">
+        <div className="footer-left">
+          <h2>Logo</h2>
+          <p>rendom here to intro llll</p>
+          <div className="social-icon"></div>
+        </div>
+        <div className="footer-right">
+          <h2>Contact Me</h2>
+          <div className="contact-info">
+            <div className="contact-here">
+              <h2>22</h2>
+              <h2>email@here.com</h2>
+            </div>
+            <div className="contact-here">
+              <h2>22</h2>
+              <h2>email@here.com</h2>
+            </div>
+          </div>
+          <div className="contact-here">
+            <h2>22</h2>
+            <h2>email@here.com</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
